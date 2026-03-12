6eb16858fc1092da884faf0686fc90dfb5098167
Noah Alguire <alguirn@sunypoly.edu>
2026-03-12 11:47:26 -0400

    Edges, Frequency

--- Exercises_Frequency.py
+++ Exercises_Frequency.py
@@ -12,18 +12,20 @@ import timm
 import torchvision
 
 mouse_down = False
+
 def on_mouse(event, x, y, flags, param):
     global mouse_down
     if event == cv2.EVENT_LBUTTONUP:
         mouse_down = False
-    elif (event == cv2.EVENT_LBUTTONDOWN or
+    elif (event == cv2.EVENT_LBUTTONDOWN or 
           (mouse_down and event == cv2.EVENT_MOUSEMOVE)):
         mouse_down = True
         mask_image = param[0]
         circle_radius = param[1]
         fill_value = 0.0
-        cv2.circle(mask_image, (x,y),
+        cv2.circle(mask_image, (x,y), 
                    circle_radius, fill_value, -1)
+        
 
 def to_numpy_complex(complex_image):
     planes = cv2.split(complex_image)
@@ -66,16 +68,17 @@ def make_simple_complex(length=600):
     #print(complex_data)
     return complex_data
 
-def compute_fourier(image):
+def compute_fourier(image, nonzeroRows=0):
     fimage = image.astype("float64")
-    return cv2.dft(fimage, flags=cv2.DFT_COMPLEX_OUTPUT)
+    return cv2.dft(fimage, flags=cv2.DFT_COMPLEX_OUTPUT, nonzeroRows=nonzeroRows)
 
-def compute_inverse_fourier(freq):
-    return cv2.idft(freq, flags=cv2.DFT_REAL_OUTPUT+cv2.DFT_SCALE)
-
+def compute_inverse_fourier(freq, nonzeroRows=0):
+    return cv2.idft(freq, flags=cv2.DFT_REAL_OUTPUT+cv2.DFT_SCALE,
+                    nonzeroRows=nonzeroRows)
+    
 def make_display_magnitude(mag):
     display_mag = cv2.log(mag + 1.0)
-    cv2.normalize(display_mag, display_mag, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+    cv2.normalize(display_mag, display_mag, norm_type=cv2.NORM_MINMAX)
     return display_mag
 
 def get_optimal_dft_size(image):
@@ -84,44 +87,42 @@ def get_optimal_dft_size(image):
     return m,n
 
 def make_gaussian_filter(side_len):
-    gaussianCol = cv2.getGaussianKernel(side_len, sigma = 0)
+    gaussianCol = cv2.getGaussianKernel(side_len, sigma=0)
     gaussianRow = np.transpose(gaussianCol)
     kernel = np.matmul(gaussianCol, gaussianRow)
     return kernel
 
 def complex_image_to_display_mag(complex_image):
-    complex_data = to_numpy_complex(complex_data)
+    complex_data = to_numpy_complex(complex_image)
     mag, _ = complex_to_polar(complex_data)
     display_mag = make_display_magnitude(mag)
     return display_mag
-
+    
 def filter_with_fourier(image, kernel):
-    padded_rows = cv2.getOptimalDFTSize(image.shape[0] + (kernel.shape[0] - 1) )
-    padded_cols = cv2.getOptimalDFTSize(image.shape[1] + kernel.shape[1])
+    padded_rows = cv2.getOptimalDFTSize(image.shape[0] + kernel.shape[0] - 1)
+    padded_cols = cv2.getOptimalDFTSize(image.shape[1] + kernel.shape[1] - 1)
     
     fimage = image.astype("float64")
     fkernel = kernel.astype("float64")
     
-    padded_image = cv2.copyMakeBorder(fimage, 0, padded_rows-image.shape[0],
+    padded_image = cv2.copyMakeBorder(fimage, 
+                                      0, padded_rows - image.shape[0], 
                                       0, padded_cols - image.shape[1],
                                       borderType=cv2.BORDER_CONSTANT,
-                                      value = 0)
-
-    padded_kernel = cv2.copyMakeBorder(fkernel, 0, padded_rows-kernel.shape[0],
+                                      value=0)
+    
+    padded_kernel = cv2.copyMakeBorder(fkernel, 
+                                      0, padded_rows - kernel.shape[0], 
                                       0, padded_cols - kernel.shape[1],
                                       borderType=cv2.BORDER_CONSTANT,
-                                      value = 0)
+                                      value=0)
     
     padded_image = image_space_shift(padded_image)
     padded_kernel = image_space_shift(padded_kernel)
     
     F = compute_fourier(padded_image)
-    H = compute_fourier(padded_kernel)
+    H = compute_fourier(padded_kernel)    
     G = cv2.mulSpectrums(F, H, 0, conjB=False)
-    
-    nG = to_numpy_complex(G)
-    mag, phase = complex_to_polar(nG)
-    display_mag = make_display_magnitude(mag)
     
     display_F = complex_image_to_display_mag(F)
     display_H = complex_image_to_display_mag(H)
@@ -130,37 +131,41 @@ def filter_with_fourier(image, kernel):
     cv2.imshow("MAGNITUDE H", display_H)
     cv2.imshow("MAGNITUDE G", display_G)
     
-    padded_output = compute_inverse_fourier(G, nonzeroRows = image.shape[0] + kernel.shape[0] - 1)
+    padded_output = compute_inverse_fourier(G, nonzeroRows=(
+                                                image.shape[0] + kernel.shape[0]-1))
     padded_output = image_space_shift(padded_output)
     
     sr = int(kernel.shape[0]/2)
     er = sr + image.shape[0]
     sc = int(kernel.shape[1]/2)
     ec = sc + image.shape[1]
-    
+        
     output = np.copy(padded_output[sr:er, sc:ec])
     output /= 255.0
-    return output    
+    return output
 
 def do_frequency(image, mask_image, draw_radius):
     '''
-    m,n = get_optimal_dft_size(image)
-    output = np.copy(image)
-    padded = cv2.copyMakeBorder(image, 0, m - image.shape[0], 0, n - image.shape[1], cv2.BORDER_CONSTANT, value=0)
+    m,n = get_optimal_dft_size(image)   
+    padded = cv2.copyMakeBorder(image, 
+                                0, m - image.shape[0],
+                                0, n - image.shape[1],
+                                borderType=cv2.BORDER_CONSTANT,
+                                value=0)
     fourier = compute_fourier(padded)
     fourier_data = to_numpy_complex(fourier)
-    mag,phase = complex_to_polar(fourier_data)
-    log_mag = cv2.log(mag + 1.0)
+    mag, phase = complex_to_polar(fourier_data)
     
-    cv2.normalize(log_mag, log_mag, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+    log_mag = make_display_magnitude(mag)
     display_phase = np.copy(phase)
     cv2.normalize(display_phase, display_phase, norm_type=cv2.NORM_MINMAX)
     
     log_mag = shift_complex(log_mag)
     mag = shift_complex(mag)
     
-    mag_window_name = cv2.namedWindow("MAGNITUDE")
-    mask_pack = [mask_image, draw_radius]
+    mag_window_name = "MAGNITUDE"
+    cv2.namedWindow(mag_window_name)
+    mask_pack = [mask_image,draw_radius]
     cv2.setMouseCallback(mag_window_name, on_mouse, mask_pack)
     
     mag *= mask_image
@@ -173,12 +178,15 @@ def do_frequency(image, mask_image, draw_radius):
     complex_data = polar_to_complex(mag, phase)
     complex_image = to_complex_image(complex_data)
     
-    output = compute_inverse_fourier(complex_data)
+    output = compute_inverse_fourier(complex_image)
     output = output[:image.shape[0], :image.shape[1]]
-    output /= 255.0
+    
+    output /= 255.0    
     
     return output
+            grayscale = cv2.cvtColor(
     '''
+    
     kernel = make_gaussian_filter(81)
     output = filter_with_fourier(image, kernel)
     return output
@@ -217,8 +225,9 @@ def main():
     print("RECON:", simple_recon)
     exit()
     '''
-    
+       
     mask_image = None
+    draw_radius = 50
            
     ###############################################################################
     # PYTORCH
@@ -269,14 +278,11 @@ def main():
             _, image = camera.read()
             grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             
-            
             if mask_image is None:
-                m,n = get_optimal_dft_size(grayscale)
+                m,n = get_optimal_dft_size(grayscale)  
                 mask_image = np.ones((m,n), dtype="float64")
-                
-            #grayscale = np.ones((17,17), dtype="uint8")
-            #mask_image = np.ones(grayscale.shape)
-            output = do_frequency(grayscale, mask_image)
+                        
+            output = do_frequency(grayscale, mask_image, draw_radius)
             
             # Show the image
             cv2.imshow(windowName, grayscale)
@@ -286,10 +292,9 @@ def main():
             key = cv2.waitKey(30)
             
             if key == ord('c'):
-                #mask_image = np.ones(grayscale.shape, dtype="float64")
-                mask_image[:,:] = 1.0
+                mask_image[:,:] = 1 # = np.ones(grayscale.shape, dtype="float64")
             elif key == ord('a'):
-                draw_radius +=5
+                draw_radius += 5
             elif key == ord('z'):
                 draw_radius -= 5
                 
@@ -310,42 +315,37 @@ def main():
         # Load image
         print("Loading image:", filename)
         image = cv2.imread(filename) 
-        grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
         # Check if data is invalid
-        if mask_image is None:
-                m,n = get_optimal_dft_size(grayscale)
-                mask_image = np.ones((m,n), dtype="float64")
-                
+        if image is None:
+            print("ERROR: Could not open or find the image!")
+            exit(1)
+
         # Show our image (with the filename as the window title)
         windowTitle = "PYTHON: " + filename
         
         ESC_KEY = 27
         key = -1
         while key != ESC_KEY:
-            
-            
+            grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             
             if mask_image is None:
-                m,n = get_optimal_dft_size(grayscale)
+                m,n = get_optimal_dft_size(grayscale)  
                 mask_image = np.ones((m,n), dtype="float64")
-                
-            #grayscale = np.ones((17,17), dtype="uint8")
-            #mask_image = np.ones(grayscale.shape)
-            output = do_frequency(grayscale, mask_image, 5)
+                        
+            output = do_frequency(grayscale, mask_image, draw_radius)
             
             # Show the image
-            cv2.imshow(windowName, grayscale)
+            cv2.imshow(windowTitle, grayscale)
             cv2.imshow("OUTPUT", output)
 
             # Wait 30 milliseconds, and grab any key presses
             key = cv2.waitKey(30)
             
             if key == ord('c'):
-                #mask_image = np.ones(grayscale.shape, dtype="float64")
-                mask_image[:,:] = 1.0
+                mask_image[:,:] = 1 # = np.ones(grayscale.shape, dtype="float64")
             elif key == ord('a'):
-                draw_radius +=5
+                draw_radius += 5
             elif key == ord('z'):
                 draw_radius -= 5
                 
@@ -356,3 +356,4 @@ def main():
 # The main function
 if __name__ == "__main__": 
     main()
+    
